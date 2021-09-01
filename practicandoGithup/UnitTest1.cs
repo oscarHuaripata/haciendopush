a1e8b6e5ab241cbda52dce433720aeb002a6541f
oscarHuaripata <osduar27@gmail.com>
2021-09-01 20:38:47 +0000

    version1

---
+++ practicandoGithup/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace practicandoGithup
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
